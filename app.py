cfbc9a187eb7fdbe7c8bba7bfd0ab381ca68581e
hzertz0 <darwenzhu1221@gmail.com>
2025-05-30 10:37:04 -0400

    test

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-for i in x:
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+# 📌 Tell Flask where the database file will be stored
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bookings.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# 📌 Create the database object
+db = SQLAlchemy(app)
